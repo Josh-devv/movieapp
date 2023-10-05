95505038870c769eb81da0beb717a12480c8c22f
joshua <joshuasofela@gmail.com>
2023-10-04 22:37:52 -0700

    testing 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { WatchlistProvider } from './components/watchlist/WatchList';
-import MovieSection from './Pages/moviesection/MovieSection'
-import MovieDesc from './Pages/moviedesc/MovieDesc';
-import SearchPage from './Pages/search/SearchPage';
-import Navbar from './components/navbar/Navbar';
-import Home from './Pages/home/Home';
-import './App.css'
-import MovieSelect from './Pages/Movies/movieselect';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { WatchlistProvider } from "./components/watchlist/WatchList";
+import MovieSection from "./Pages/moviesection/MovieSection";
+import MovieDesc from "./Pages/moviedesc/MovieDesc";
+import SearchPage from "./Pages/search/SearchPage";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./Pages/home/Home";
+import "./App.css";
+//import MovieSelect from "./Pages/Movies/movieselect";
 
 const App = () => {
-    return (
-      <>
-      
-      
-        <WatchlistProvider>  
-          <Router>
-            <Routes>   
-                  
-              <Route exact path='/' Component={MovieSection}/>
-              <Route path='/movie/:id' Component={MovieDesc}/>
-              <Route path='/search/:title' Component={SearchPage}/>    
-              <Route path='/movies' Component={MovieSelect} />    
-              
-            </Routes>
-          </Router>
-        </WatchlistProvider>  
-       </> 
-        
-           
-  )
-}
+  return (
+    <>
+      <WatchlistProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" Component={MovieSection} />
+            <Route path="/movie/:id" Component={MovieDesc} />
+            <Route path="/search/:title" Component={SearchPage} />
+            <Route path="/movies" Component={MovieSelect} />
+          </Routes>
+        </Router>
+      </WatchlistProvider>
+    </>
+  );
+};
 export default App;
